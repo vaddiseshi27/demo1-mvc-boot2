72a27421f6f0e4a7e6ac40900cc5d9e2301089bf
vaddiseshi27 <105627373+vaddiseshi27@users.noreply.github.com>
2022-07-15 22:00:14 +0530

    updated

--- src/main/java/com/jlcindia/springboot/MyBootApplication.java
+++ src/main/java/com/jlcindia/springboot/MyBootApplication.java
@@ -8,7 +8,7 @@ public class MyBootApplication {
 
 	
 	public static void main(String[] args) {
-		system.out.println("upadeted 1");
+		
 		
 		
 		SpringApplication.run(MyBootApplication.class, args);
